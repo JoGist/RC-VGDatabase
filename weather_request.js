ba4620c4354a8a6dc11ecc31546d65445a2381de
Giovanni Roma <r.giovanni.98@gmail.com>
2020-04-01 18:36:37 +0200

    Update weather_request.js

--- weather_request.js
+++ weather_request.js
@@ -1,16 +1,25 @@
+//before running the code, be sure to execute the following tasks:
+//1. commands on terminal (in the project root folder):
+//   npm init --yes
+//   npm install request --save
+//   npm install dotenv --save
+//2. create a .env file with openWeatherAPI={yourAPIkey}
+
 require('dotenv').config()
 
 var request = require ('request');
 
 var options = {
-    url: 'http://api.openweathermap.org/data/2.5/weather?q=Rome,IT&appid='+process.env.openWeatherAPI,
+    url: 'http://api.openweathermap.org/data/2.5/weather?q=Rome,IT&units=metric&appid='+process.env.openWeatherAPI,
 }
 
 function callback(error, response, body) {
+    console.log('Code '+response.statusCode);
     if(!error && response.statusCode == 200) {
         var info = JSON.parse(body);
-        console.log("##############################");
+        console.log('##############################');
         console.log(info);
+        console.log('##############################');
     }
 }
 
