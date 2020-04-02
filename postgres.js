bddcbc90af0006893ede9052c0b0d5a399b66d65
Giovanni Roma <r.giovanni.98@gmail.com>
2020-04-02 10:36:03 +0200

    await changes

--- postgres.js
+++ postgres.js
@@ -2,48 +2,19 @@ const postgres = require('postgres')
 
 const sql = postgres('postgres://postgres:biar@localhost:5432/centraline_rilevamento')
 
-//const columns = ['id', 'nome']
-
-console.log('computing query');
-
-async function query() {
-  let promise = new Promise((res, rej) => {
-    setTimeout(() => res("Now it's done!"), 1000)
+function wait() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 1000);
   });
-
-  // wait until the promise returns us a value
-  let result = await sql`
-  select id, nome from citta
-`;
-
-  // "Now it's done!"
-  alert(result);
-};
-query();
-
-
-/*function query() {
-  sql`
-  select ${
-    sql(columns)
-  } from citta
-`
-console.log(sql);
-  return sql;
 }
 
-
-let search = 'Lazio'
-
-const users = await sql`
-  select
-    id,
-    nome,
-    regione
-  from citta
-  where
-    name like ${ search + '%' }
-`*/
-
-// Is translated into this query:
-//select name, age from users
+async function query() {
+  console.log('calling');
+  const result = await sql` //await wait();
+  select * from citta
+`;
+  console.log(result);
+}
+query();
