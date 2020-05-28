552b463b249f61113ef423240f44bbbfb25a6335
Loldlink <the.anarky98@gmail.com>
2020-05-28 16:41:26 +0200

    api

--- VGDatabase/app/controllers/api_controller.rb
+++ VGDatabase/app/controllers/api_controller.rb
@@ -2,12 +2,12 @@ class ApiController < ApplicationController
     skip_before_action :verify_authenticity_token
     skip_before_action :set_current_user
 
-    def apiRest
-        id = params[:id]
-        @games = Game.where(:title => id)
+    def apiRest #Gioco con quel titolo
+        title = params[:title]
+        @games = Game.where(:title => title)
         render json: @games
     end
-    def apiRest2
+    def apiRest2 #Mostra utente tramite id/email/username
         key = params[:key]
         id = params[:id]
         username = params[:username]
@@ -23,13 +23,13 @@ class ApiController < ApplicationController
                 @user = User.select(:id,:username,:email,:google_token,:steam_token,:background,:avatar,:social1,:social2,:social3).where(:email => mail)[0]
                 render json: @user
             else
-                render json: []
+                render json: 404
             end
         else
             render json: 401
         end
     end
-    def apiRest3
+    def apiRest3 #Mostra le review dell'utente o di un gioco specifico tramite id
         game_id = params[:game_id]
         user_id = params[:user_id]
         key = params[:key]
@@ -41,7 +41,7 @@ class ApiController < ApplicationController
                 @re2 = Review.where(:game_id => User.find(game_id).id)
                 render json: @re2
             else
-                render json: []
+                render json: 404
             end
         elsif key==''
             if game_id!=nil && Game.exists?(:id => game_id)
@@ -52,7 +52,7 @@ class ApiController < ApplicationController
             render json: 401
         end
     end
-    def apiRest4
+    def apiRest4 #Amici di un dato utente
         user_id = params[:user_id]
         key = params[:key]
         if key=='123456789'
@@ -66,7 +66,7 @@ class ApiController < ApplicationController
             render json: 401
         end
     end
-    def apiRest5
+    def apiRest5 #Collezione di un dato utente
         user_id = params[:user_id]
         key = params[:key]
         if key=='123456789'
@@ -81,14 +81,14 @@ class ApiController < ApplicationController
         end
     end
 
-    def apiRest6
+    def apiRest6 #Creazione di un utente
         username = params[:username]
         password = params[:password]
         email = params[:email]
         key = params[:key]
         if key=='123456789'
-            if !User.exists?(:username => username)
-                User.create(:username => username, :password => password, :email => email)
+            if !User.exists?(:username => username, :email => email)
+                User.create(:username => username, :password => password, :email => email, :avatar => 'Avatars/avatar_0',:background => 'deafult.png', :social1 => '', :social2 => '', :social3 => '')
                 render json: User.find(User.where(:username => username)[0].id)
             else
                 render json: 'User already exists'
@@ -98,33 +98,33 @@ class ApiController < ApplicationController
         end
     end
 
-    def apiRest7
-        user_id = params[:user_id]
+    def apiRest7 #Eliminazione di un dato utente
+        username = params[:username]
         key = params[:key]
         if key=='123456789'
-            if user_id!=nil && User.exists?(:id => user_id)
-                User.delete(user_id)
-                render json: User.where(:id => user_id)
+            if User.exists?(:username => username)
+                User.delete(User.where(:username => username)[0].id)
+                render json: 200
             else
-                render json: 'User not found'
+                render json: 404
             end
         else
             render json: 401
         end
     end
 
-    def apiRest8
-        user_id = params[:user_id]
-        username = params[:username]
+    def apiRest8 #Modifica di alcuni attributi del profilo di un utente
+        usernameold = params[:usernameold]
+        usernamenew = params[:usernamenew]
         password = params[:password]
         email = params[:email]
         key = params[:key]
         if key=='123456789'
-            if user_id!=nil && User.exists?(:id => user_id)
-                User.find(user_id).update_attributes!(:username => username, :password => password, :email => email)
-                render json: User.find(User.where(:username => username)[0].id)
+            if User.exists?(:username => usernameold)
+                User.where(:username => usernameold)[0].update_attributes!(:username => usernamenew, :password => password, :email => email)
+                render json: User.find(User.where(:username => usernamenew)[0].id)
             else
-                render json: 'User not found'
+                render json: 404
             end
         else
             render json: 401
