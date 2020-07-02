99d972da1fd0191c7b250dcf1ad59a3b60d17ab2
Giovanni Roma <r.giovanni.98@gmail.com>
2020-07-02 17:05:53 +0200

    Fixed db

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,24 @@ ActiveRecord::Schema.define(version: 2020_06_30_142909) do
     t.string "avatar"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "username"
+    t.string "password"
+    t.string "avatar"
+    t.string "background"
+    t.string "location"
+    t.string "social1"
+    t.string "social2"
+    t.string "social3"
+    t.float "latitude"
+    t.float "longitude"
+    t.decimal "google_token"
+    t.string "google_username"
+    t.string "steam_username"
+    t.bigint "steam_token"
+  end
+
   create_table "friends", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "friend_id"
@@ -74,22 +92,5 @@ ActiveRecord::Schema.define(version: 2020_06_30_142909) do
     t.index ["user_id"], name: "index_stores_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "username"
-    t.string "password"
-    t.string "avatar"
-    t.string "background"
-    t.string "location"
-    t.string "social1"
-    t.string "social2"
-    t.string "social3"
-    t.float "latitude"
-    t.float "longitude"
-    t.decimal "google_token"
-    t.string "google_username"
-    t.string "steam_username"
-    t.bigint "steam_token"
-  end
 
 end
