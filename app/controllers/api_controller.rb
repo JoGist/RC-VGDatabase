5c0dd9e2185695a6d8f4e8de35e022eeaf5f8542
Giovanni Roma <r.giovanni.98@gmail.com>
2020-07-06 11:50:56 +0200

    Small fixes

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,15 +3,20 @@ class ApiController < ApplicationController
     skip_before_action :set_current_user
 
     def apiRest #Gioco con quel titolo get
+        key = params[:key]
         title = params[:title]
-        @games = Game.where(:title => title)
-        render json: @games
+        if key=='123456789'
+            @games = Game.where(:title => title)
+            render json: @games
+        else
+            render json: 401
+        end
     end
     def apiRest2 #Mostra utente tramite id/email/username get
         key = params[:key]
         id = params[:id]
         username = params[:username]
-        mail = params[:mail]
+        email = params[:email]
         if key=='123456789'
             if id.length!=0 && User.exists?(:id => id)
                 @user = User.select(:id,:username,:email,:google_token,:steam_token,:background,:avatar,:social1,:social2,:social3).where(:id => id)[0]
@@ -19,8 +24,8 @@ class ApiController < ApplicationController
             elsif username!=nil && User.exists?(:username => username)
                 @user = User.select(:id,:username,:email,:google_token,:steam_token,:background,:avatar,:social1,:social2,:social3).where(:username => username)[0]
                 render json: @user
-            elsif mail!=nil && User.exists?(:email => mail)
-                @user = User.select(:id,:username,:email,:google_token,:steam_token,:background,:avatar,:social1,:social2,:social3).where(:email => mail)[0]
+            elsif email!=nil && User.exists?(:email => email)
+                @user = User.select(:id,:username,:email,:google_token,:steam_token,:background,:avatar,:social1,:social2,:social3).where(:email => email)[0]
                 render json: @user
             else
                 render json: 404
@@ -30,9 +35,9 @@ class ApiController < ApplicationController
         end
     end
     def apiRest3 #Mostra le review dell'utente o di un gioco specifico tramite id get
+        key = params[:key]
         game_id = params[:game_id]
         user_id = params[:user_id]
-        key = params[:key]
         if key=='123456789'
             if user_id!=nil && User.exists?(:id => user_id)
                 @re1 = Review.where(:user_id => User.find(user_id).id)
@@ -53,8 +58,8 @@ class ApiController < ApplicationController
         end
     end
     def apiRest4 #Amici di un dato utente get
-        user_id = params[:user_id]
         key = params[:key]
+        user_id = params[:user_id]
         if key=='123456789'
             if user_id!=nil && User.exists?(:id => user_id)
                 @f1 = Friend.where(:user_id => User.find(user_id).id)
@@ -67,8 +72,8 @@ class ApiController < ApplicationController
         end
     end
     def apiRest5 #Collezione di un dato utente get
-        user_id = params[:user_id]
         key = params[:key]
+        user_id = params[:user_id]
         if key=='123456789'
             if user_id!=nil && User.exists?(:id => user_id)
                 @m1 = Store.where(:user_id => User.find(user_id).id)
@@ -82,10 +87,10 @@ class ApiController < ApplicationController
     end
 
     def apiRest6 #Creazione di un utente post
-        username = params[:username]
-        password = params[:password]
-        email = params[:email]
         key = params[:key]
+        username = params[:username]
+        email = params[:email]
+        password = params[:password]
         if key=='123456789'
             if !User.exists?(:username => username, :email => email)
                 User.create(:username => username, :password => password, :email => email, :avatar => 'Avatars/avatar_0',:background => 'deafult.png', :social1 => '', :social2 => '', :social3 => '')
@@ -99,8 +104,8 @@ class ApiController < ApplicationController
     end
 
     def apiRest7 #Eliminazione di un dato utente delete
-        username = params[:username]
         key = params[:key]
+        username = params[:username]
         if key=='123456789'
             if User.exists?(:username => username)
                 User.delete(User.where(:username => username)[0].id)
@@ -114,14 +119,13 @@ class ApiController < ApplicationController
     end
 
     def apiRest8 #Modifica di alcuni attributi del profilo di un utente put
+        key = params[:key]
         usernameold = params[:usernameold]
         usernamenew = params[:usernamenew]
         password = params[:password]
-        email = params[:email]
-        key = params[:key]
         if key=='123456789'
             if User.exists?(:username => usernameold)
-                User.where(:username => usernameold)[0].update_attributes!(:username => usernamenew, :password => password, :email => email)
+                User.where(:username => usernameold)[0].update_attributes!(:username => usernamenew, :password => password)
                 render json: User.find(User.where(:username => usernamenew)[0].id)
             else
                 render json: 404
